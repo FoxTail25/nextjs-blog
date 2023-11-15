72169c7d2bbfca18e55e1c3ac28cbd1db31d2f5a
Fox Tail <foxtail25@gmail.com>
2023-11-15 17:35:58 +0300

    step1off

---
+++ components/Component2I.js
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+
+const Component2I = () => (
+    <Image
+        src="/profile.jpg"
+        height={144}
+        width={144}
+        alt="test"
+    />
+)
+
+export default Component2I
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,31 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
+import Component2I from '../../components/Component2I';
+import s from '../../styles/first.module.scss'
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+
+      <Head>
+        <title>Firat Page</title>
+        <link rel="icon" href="/favicon.ico" />
+        {/* <script src='/firstp.js'></script> */}
+
+      </Head>
+
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+      {/* <Component2I /> */}
+      <p className={s.text}>
+        some text 
+        <span>some text in span</span>
+      </p>
+    </Layout>
   );
 }
